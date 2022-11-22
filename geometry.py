55f53ae81841577dd953cc6d3ef6483790985060
sara mathew <smathew@blueorigin.com>
2022-11-22 12:25:31 -0800

    refactored with classes

--- geometry.py
+++ geometry.py
@@ -1,21 +1,23 @@
 # area and perimeter for shapes
 import math
 
-def area_circle(r):
-    return math.pi*(r**2)
+class Circle:
+    def area(self,r):
+        return math.pi*(r**2)
 
-def perimeter_circle(r):
-    return 2*r*math.pi
+    def perimeter(self,r):
+        return 2*r*math.pi
 
-def area_rectangle(w,h):
-    return w*h
+class Rectangle:
+    def area(self,w,h):
+        return w*h
 
-def perimeter_rectangle(w,h):
-    return 2*(w+h)
+    def perimeter(self,w,h):
+        return 2*(w+h)
 
-def area_triangle(b,h):
-    return 0.5*b*h
+class Triangle:
+    def area(self,b,h):
+        return 0.5*b*h
 
-def perimeter_triangle(b,h):
-    return b+h
-    
+    def perimeter(self,b,h):
+        return b+h
--- test_geometry.py
+++ test_geometry.py
@@ -1,21 +1,20 @@
-from geometry import area_circle, area_rectangle, area_triangle
-from geometry import perimeter_circle, perimeter_rectangle, perimeter_triangle
+from geometry import Circle, Rectangle, Triangle
 import math
 
 def test_area_circle():
-    assert area_circle(2) == 4*math.pi
+    assert Circle().area(2) == 4*math.pi
 
 def test_perimeter_circle():
-    assert perimeter_circle(2) == 4*math.pi
+    assert Circle().perimeter(2) == 4*math.pi
 
 def test_area_triangle():
-    assert area_triangle(3,4) == 6
+    assert Triangle().area(3,4) == 6
 
 def test_perimeter_triangle():
-    assert perimeter_triangle(2,3) == 5
+    assert Triangle().perimeter(2,3) == 5
 
 def test_area_rectangle():
-    assert area_rectangle(3,4) == 12
+    assert Rectangle().area(3,4) == 12
 
 def test_perimeter_rectangle():
-    assert perimeter_rectangle(2,3) == 10
+    assert Rectangle().perimeter(2,3) == 10
